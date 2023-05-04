037489142fbd4aff5ae19362e599bf042e3c7add
Luis Henrique Braga de Sousa <luishenrinquebraga@gmail.com>
2023-05-03 21:12:45 -0300

    fix: some configs of prettier for accept ; in the end of lines

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
-import app from './app'
+import app from './app';
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => { console.log(`Running on ${PORT}`) });
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
